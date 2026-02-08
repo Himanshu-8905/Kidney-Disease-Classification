c5720e63a5abe09af807bbda10380a391af7f147
Himanshu-8905 <kumawatbasant555@gmail.com>
2026-02-08 22:16:21 +0530

    update configuration file

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from src.cnnClassifier.constants import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
-from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig, TrainingConfig)
+from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig, TrainingConfig , EvaluationConfig)
 import os
 
 class ConfigurationManager:
@@ -66,6 +66,18 @@ class ConfigurationManager:
             params_image_size = params.IMAGE_SIZE
         )
         return training_config
+    
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-images",
+            mlflow_uri="https://dagshub.com/Himanshu-8905/Kidney-Disease-Classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
     
     
 
